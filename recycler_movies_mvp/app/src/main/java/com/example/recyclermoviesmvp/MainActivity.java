97293d5146849cb0d49438aced35c93f5903684c
Alberto Hernández Akkari <akkarihdez@gmail.com>
2024-10-28 13:21:06 +0100

    [THREADS] IMÁGENES 28-10-24
    [THREADS] IMÁGENES 28-10-24

--- recycler_movies_mvp/app/src/main/java/com/example/recyclermoviesmvp/MainActivity.java
+++ recycler_movies_mvp/app/src/main/java/com/example/recyclermoviesmvp/MainActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.recyclermoviesmvp.json_mapper.Movie;
 import com.example.recyclermoviesmvp.json_mapper.MovieResponse;
 import com.example.recyclermoviesmvp.retrofit.RetrofitClient;
+import com.example.recyclermoviesmvp.utils._SV2_INF_THE_BEST;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import retrofit2.Call;
@@ -18,15 +27,61 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView imagen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initRecyclerView();
-        // initRetrofit();
+        imagen = findViewById(R.id.imgPortada);
+        //initRecyclerView();
+        // initThreadImageView();
+        //initGlide();
+         initSVTeamImageView();
     }
+
+    private void initSVTeamImageView() {
+        String myUrl = "https://lh4.googleusercontent.com/proxy/JG73x59mTNYlvZ5cQsk9mBag4NiNL_O58q4YC0DtyiSsEw8W2iQcAZhYTyEPbfr1DM3CWbT-LgJ8T8QDy6tKjRGHODw4UDQgN9ZF-rta-ifXdWwCmqw";
+        _SV2_INF_THE_BEST.Builder builder = new _SV2_INF_THE_BEST.Builder(myUrl, imagen);
+
+        // builder.generateImage().build();
+        builder.build().load();
+
+    }
+
+    private void initGlide() {
+        // Simplemente usa Glide para cargar la imagen en el ImageView
+        Glide.with(getBaseContext())
+                .load("https://lh4.googleusercontent.com/proxy/JG73x59mTNYlvZ5cQsk9mBag4NiNL_O58q4YC0DtyiSsEw8W2iQcAZhYTyEPbfr1DM3CWbT-LgJ8T8QDy6tKjRGHODw4UDQgN9ZF-rta-ifXdWwCmqw")
+                //.placeholder(R.drawable.placeholder) // Imagen temporal mientras carga
+                //.error(R.drawable.error) // Imagen en caso de error de carga
+                .into(imagen); // Cargar la imagen en el ImageView
+    }
+
+    // URL => https://w7.pngwing.com/pngs/379/413/png-transparent-300-logo-sparta-film-logo-blood-love-miscellaneous-text-thumbnail.png
+    static Bitmap bitmap;
+    private void initThreadImageView() {
+        new Thread(() ->{
+            try {
+                URL url = new URL("https://lh4.googleusercontent.com/proxy/JG73x59mTNYlvZ5cQsk9mBag4NiNL_O58q4YC0DtyiSsEw8W2iQcAZhYTyEPbfr1DM3CWbT-LgJ8T8QDy6tKjRGHODw4UDQgN9ZF-rta-ifXdWwCmqw");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(input);
+
+                Runnable actualizarUI =new Runnable() {
+                    @Override
+                    public void run() {
+                        imagen.setImageBitmap(bitmap);
+                    }
+                };
+                runOnUiThread(actualizarUI);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }).start();
+    }
+
     private RecyclerView recyclerView;
     // private PeliculaAdapter1 adapter;
 
